f3e3a8a50f1896786d07bdc2ea4b6d2398588bfb
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2022-01-31 21:17:42 -0500

    1.0.6

---
+++ R/data.R
@@ -0,0 +1,30 @@
+
+#' Cell counts following interferon treatment
+#'
+#' Counts are from single cell RNA-seq data from treated and untreated samples from \insertCite{kang2018multiplexed}{crumblr}.
+#'
+#' @docType data
+#'
+#' @usage data(IFNCellCounts)
+#'
+#' @format 
+#' \itemize{    
+#'	\item cellCounts data.frame of cell counts.
+#'	\item `info` is metadata for each sample
+#' }
+#'
+#' @keywords datasets
+#'
+#' @name IFNCellCounts
+#' @examples
+#' data(IFNCellCounts)
+"info"
+
+
+
+
+
+
+
+#' @importFrom tibble tibble
+NULL
