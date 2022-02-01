da4239faa01a3e02934b809ee7674f049e4734a7
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2022-02-01 09:38:52 -0500

    update docs

--- R/data.R
+++ R/data.R
@@ -9,35 +9,18 @@
 #'
 #' @format 
 #' \itemize{    
-#'	\item `cellCounts` data.frame of cell counts.
-#'	\item `info` is metadata for each sample
+#'	\item \code{cellCounts} data.frame of cell counts.
+#'	\item \code{info} is metadata for each sample
 #' }
 #'
 #' @keywords datasets
 #'
+#' @references{
+#'   \insertRef{kang2018multiplexed}{crumblr}
+#' }
 #' @name IFNCellCounts
 #' @examples
 #' data(IFNCellCounts)
 "info"
-
-#' Cell counts following interferon treatment
-#'
-#' Counts are from single cell RNA-seq data from treated and untreated samples from \insertCite{kang2018multiplexed}{crumblr}.
-#'
-#' @docType data
-#'
-#' @usage data(IFNCellCounts)
-#'
-#' @format 
-#' \itemize{    
-#'	\item `cellCounts` data.frame of cell counts.
-#'	\item `info` is metadata for each sample
-#' }
-#'
-#' @keywords datasets
-#'
-#' @name IFNCellCounts
-#' @examples
-#' data(IFNCellCounts)
 "cellCounts"
 
