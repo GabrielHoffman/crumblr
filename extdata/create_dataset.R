5758c37d91a97c62e130803a0f191c8f63addf59
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2025-02-07 11:56:34 -0500

    add script for example data

---
+++ extdata/create_dataset.R
@@ -0,0 +1,47 @@
+library(dreamlet)
+library(ExperimentHub)
+library(scater)
+
+# Download data, specifying EH2259 for the Kang, et al study
+eh <- ExperimentHub()
+sce <- eh[["EH2259"]]
+
+sce$ind = as.character(sce$ind)
+
+# only keep singlet cells with sufficient reads
+sce <- sce[rowSums(counts(sce) > 0) > 0, ]
+sce <- sce[, colData(sce)$multiplets == "singlet"]
+
+# compute QC metrics
+qc <- perCellQCMetrics(sce)
+
+# remove cells with few or many detected genes
+ol <- isOutlier(metric = qc$detected, nmads = 2, log = TRUE)
+sce <- sce[, !ol]
+
+# set variable indicating stimulated (stim) or control (ctrl)
+sce$StimStatus <- sce$stim
+
+# Since 'ind' is the individual and 'StimStatus' is the stimulus status,
+# create unique identifier for each sample
+sce$id <- paste0(sce$StimStatus, sce$ind)
+
+# Create pseudobulk data by specifying cluster_id and sample_id
+# Count data for each cell type is then stored in the `assay` field
+# assay: entry in assayNames(sce) storing raw counts
+# cluster_id: variable in colData(sce) indicating cell clusters
+# sample_id: variable in colData(sce) indicating sample id for aggregating cells
+pb <- aggregateToPseudoBulk(sce,
+  assay = "counts",
+  cluster_id = "cell",
+  sample_id = "id",
+  verbose = FALSE
+)
+
+df_cellCounts = cellCounts(pb)
+
+info = as.data.frame(colData(pb))
+
+hcl <- buildClusterTreeFromPB(pb)
+
+save(df_cellCounts, info, hcl, file="IFNCellCounts.rda")
